1e6dee531cff094dc0628926f34dc543e40a4800
mohab fouda <153132608+MohabFouda20@users.noreply.github.com>
2024-03-07 21:47:52 +0200

    fjkajsf

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,8 +5,8 @@ import requests
 endpoint = "http://localhost:8000/api/"
 
 
-respone = requests.get(endpoint) # Application Programming Interface (API) simple one 
-print (respone.json()['message']) 
+respone = requests.get(endpoint , params={"abc": 123}) # Application Programming Interface (API) simple one 
+print (respone.json()) 
 
 
 
