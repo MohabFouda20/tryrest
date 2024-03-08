c24a751abef22590bec316eb51896af2234b2d56
mohab fouda <153132608+MohabFouda20@users.noreply.github.com>
2024-03-09 01:08:48 +0200

    change the dict woth the form models

--- backend/api/views.py
+++ backend/api/views.py
@@ -28,9 +28,11 @@ def api_home(request , *args , **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        #  before using the model_to_dict
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
         # 
+        data = model_to_dict(model_data)
     return JsonResponse(data)
